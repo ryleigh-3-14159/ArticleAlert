35747fe399eb2acb8e768daca17c5c7eb0c8553f
Ryleigh <ryleigh.s.314159@gmail.com>
2021-02-16 15:37:42 -0800

    Added text message logic, opened client API connection, tested valid delivery, and tied execution into main function.

--- scraper.py
+++ scraper.py
@@ -35,7 +35,35 @@ def pick_article(article_store):
     """
     return text_string
 
+# opens twilio client api and sends sms message to specified number
+def send_message(message):
+    # creates client object from twilio account and api auth token
+    client = Client(ACCOUNT_ID, AUTH_TOKEN)
+    # sends message
+    sms = client.messages \
+        .create(
+        body=message,
+        from_='+18312188286',
+        to='+14153195470'
+    )
+
 def main():
+    headers = {"user-agent": USER_AGENT}
+    news_pages = ["space", "nanotech", "physics", "earth", "technology", "chemistry", "biology", "science"]
+    topic = choice(news_pages)
+
+    # concatenates random news topic onto url
+    URL = "https://phys.org/" + topic + "-news/"
+
+    # obtains pure html from request using headers as a mask, sleeps for three seconds prior
+    sleep(3)
+    response = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    art = soup.find_all("article")
+    front_page_articles = parse_article(art)
+    sms_message = pick_article(front_page_articles)
+    send_message(sms_message)
 
 
 if __name__ == '__main__':
