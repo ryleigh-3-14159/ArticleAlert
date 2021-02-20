fd1477d5c97852ee2a016350ad489e3661693288
Ryleigh <ryleigh.s.314159@gmail.com>
2021-02-19 19:10:18 -0800

    Clarified Comments

--- source/sms.py
+++ source/sms.py
@@ -5,20 +5,22 @@ NUMBER = 'YOUR NUMBER'
 # SMS gateways for each carrier.
 # the google voice is specific to the conversation between the receiving number and the GV number
 carriers = {
-    'att': '@mms.att.net',
-    'tmobile': '@tmomail.net',
+    'at_and_t': '@mms.att.net',
     'verizon': '@vtext.com',
     'sprint': '@page.nextel.com',
+    't_mobile': '@tmomail.net',
     'google_voice': 'text_thrad_code@txt.voice.google.com'
 }
 
 def send(message):
-    # Replace the number with your own
+    # Replace the number with your own GV number
     to_number = 'GOOGLE_VOICE_NUMBER.{}{}'.format(NUMBER, carriers['google_voice'])
+    # UNCOMMENT FOR NON-GOOGLE VOICE NUMBER
+    # to_number = f'Your Number{carriers["YOUR CARRIER"]}'
 
     # authentication using specific google credentials,
     # note that if using google voice, this must be the same email used for voice number
-    auth = ('romynashwood', 'Lunafazes15')
+    auth = ('YOUR GMAIL USERNAME', 'YOUR PASSWORD')
 
     # Establish a secure session with gmail's outgoing SMTP server using your gmail account
     server = smtplib.SMTP("smtp.gmail.com", 587)
