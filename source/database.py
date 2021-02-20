893d4721b91beedbd73a41727a64e7d76de824ac
Ryleigh <ryleigh.s.314159@gmail.com>
2021-02-19 19:44:19 -0800

    Cleaned up database code

--- source/database.py
+++ source/database.py
@@ -1,12 +1,11 @@
-# !!!UNCOMMENT FOR DATABASE CREATION!!!
 
-# import sqlite3
-#
-# conn = sqlite3.connect("article_data.db")
-# # creates cursor
-# c = conn.cursor()
-#
-# c.execute('''CREATE TABLE articles (title TEXT, href TEXT, topic TEXT, date_input TEXT)''')
-# # commit changes
-# conn.commit()
-# conn.close()
+import sqlite3
+
+conn = sqlite3.connect("article_data.db")
+# creates cursor
+c = conn.cursor()
+
+c.execute('''CREATE TABLE articles (title TEXT, href TEXT, topic TEXT, date_input TEXT)''')
+# commit changes
+conn.commit()
+conn.close()
